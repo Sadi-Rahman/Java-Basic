6465db81fff6ac2901ac82d4605ef740460e25d5
Sadi-Rahman <sadiakash1309@gmail.com>
2022-11-23 17:26:46 +0600

    Basic

---
+++ for/main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class main{
+public static void main(String[] args) {
+
+    int sum=0;
+    int n = 5;
+    for (int i = 0; i <= n; i++) {
+         System.out.println("Printed");
+    }
+
+    for (int i = 1; i <= n; i++) {
+         System.out.println(i);
+    }
+
+    for (int i = 1; i <=n; i++) {
+        sum += i;
+    }
+        System.out.println("Sum " +sum);
+ }
+}; 
+    
+
