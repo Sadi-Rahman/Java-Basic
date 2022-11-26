29ff6ab584b333fc9cea9fb38ceb6b7bf730fe4c
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2022-11-26 20:08:19 +0600

    Basic

---
+++ Length/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Main{
+public static void main(String[] args) {
+        String w1 = "Name";
+        System.out.println("Example: The Lenght Of The Word NAME is: " + w1.length());
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your word ");
+        String w2 =sc.next();
+         System.out.println("Length of your word is " + w2.length());
+ }
+};
