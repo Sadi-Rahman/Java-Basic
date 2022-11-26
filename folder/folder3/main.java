7367bc1ae1ec90aa479d9594619d8e21a48d1fb0
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2022-11-26 17:07:51 +0600

    Basic

---
+++ folder/folder3/main.java
@@ -0,0 +1,42 @@
+import java.util.*;
+class Login{
+     Scanner sc;
+     String uname,pass;
+
+     public Login(Scanner sc){
+        this.sc=sc;
+        insertData();
+     }
+
+
+public void insertData() {
+     System.out.println("Enter Username: ");
+     uname=sc.nextLine();
+
+      System.out.println("Enter Password: ");
+      pass=sc.nextLine();
+
+      valid();
+    
+    }
+public void valid() {
+    if(uname.equals("admin")){
+        if (pass.equals("admin")) {
+             System.out.println("Login Successfull");
+           }
+           else{
+             System.out.println("Wrong Password");
+           }
+    }
+    else{
+         System.out.println("Wrong Username");
+        
+        } 
+    }
+}
+class main{
+public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
+ Login l=new Login(sc);
+ }
+};
