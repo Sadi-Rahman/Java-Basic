83d8631e0715066054e8d8cdccced6198a383730
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2022-11-30 14:40:41 +0600

    Basic

---
+++ folder/folder4/main.java
@@ -0,0 +1,12 @@
+package folder4;
+
+import java.util.*;
+class main{
+    static main reg =new Register();
+    public static void main(String[] args) {
+        try ()
+     
+     
+     
+ }
+};
---
+++ folder/folder6/main.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+
+class main{
+public static void main(String[] args) {
+
+    String s;
+    int a;
+    double num, num1, num2;
+    double res =0;
+    Scanner sc = new Scanner(System.in);
+    
+  
+//    do{
+ 
+    System.out.println("[1] Addition  [2] Subtraction  [3] Multiplication\n[4] Division  [5] Percentage   [6] Exit");
+    System.out.println("Please Enter Your Operation");
+
+    
+    s = sc.nextLine();
+     if (s.equals("1")) {
+
+        System.out.println("How Many Inputs: ");
+        a = sc.nextInt();
+        System.out.println("Enter "+a+" Number");
+        for (int i = 0; i < a; i++) {
+            num = sc.nextDouble();
+            res +=num;   
+        }            
+            System.out.println("Result: "+res);
+
+     } 
+     else if (s.equals("2")) {
+           
+            System.out.println("Enter Number 1 ");
+            num1 =sc.nextDouble();
+            System.out.println("Enter Number 2 ");
+            num2 =sc.nextDouble();
+            res=num1-num2;
+            System.out.println("Result: "+res);
+     } 
+     else if(s.equals("3")){
+
+            System.out.println("Enter Number 1 ");
+            num1 =sc.nextDouble();
+            System.out.println("Enter Number 2 ");
+            num2 =sc.nextDouble();
+            res=num1*num2;
+            System.out.println("Result: "+res);
+     }
+     else if(s.equals("4")){
+
+            System.out.println("Enter Number 1 ");
+            num1 =sc.nextDouble();
+            System.out.println("Enter Number 2 ");
+            num2 =sc.nextDouble();
+            res=num1/num2;
+            System.out.println("Result: "+res);
+     }
+     else if(s.equals("5")){
+
+            System.out.println("Enter Number 1 ");
+            num1 =sc.nextDouble();
+            System.out.println("Enter Number 2 ");
+            num2 =sc.nextDouble();
+            res=((num1/num2)*100);
+            System.out.println("Result: "+res );
+     }
+     else if(s.equals("6")){
+
+            System.out.println("Exiting");
+            System.exit(0);
+     }
+
+     else {
+         System.out.println("Error");
+        // 
+     }  
+    //  System.out.println("Do You Want To Continue [yes]");
+    //  s=sc.next();
+    // }
+    //  while (s.equalsIgnoreCase("yes"));
+      sc.close();
+}
+};
