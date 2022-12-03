0e1c491c55adef290bfc34562f03efeb170f660c
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2022-12-03 17:20:54 +0600

    Basic

--- thread/th3/Multithread.java
+++ thread/th3/Multithread.java
@@ -1,5 +1,24 @@
 package thread.th3;
 
+class Threading extends Thread{
+    @Override
+    public void run() {
+        try {
+             System.out.println("Thread" + Thread.currentThread().getId() + "Running");
+        } catch (Exception e) {
+             System.out.println("Exception");
+        }        
+    }
+
+}
+
 public class Multithread {
+    public static void main(String[] args) {
+        int n =5;
+        for (int i = 0; i < n; i++) {
+            Threading obj = new Threading();
+            obj.start();
+        }
+    }
     
 }
