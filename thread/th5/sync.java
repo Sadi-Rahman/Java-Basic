c200ccd0c4972d06b3e9780f00d5e42f39af40d8
Md. Abu Bakar Siddique Sadi <sadiakash1309@gmail.com>
2022-12-04 19:54:04 +0600

    Basic

--- thread/th5/sync.java
+++ thread/th5/sync.java
@@ -1,16 +1,56 @@
 import java.util.*;
 
 public class sync {
+
     static int count=0;
-    Scanner sc = new Scanner(System.in);
-    int c;
+    static  Scanner sc = new Scanner(System.in);
+    static int c;
 
     public synchronized static void incount(){
         count++;
+
     }
+
 
     public static void main(String[] args) {
-         System.out.println("Enter The Number Of Tickets");
-    }
 
+         System.out.println("Number Of Tickets");
+         c=sc.nextInt();
+
+         if (c>2000) {
+             System.out.println("Error Number of Tickets is too high");
+             System.exit(0);
+         }
+
+         Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    incount();
+                }
+            }
+         });
+
+         Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    incount();
+                }
+            }
+         });
+
+         t1.start();
+         t2.start();
+
+         try {
+             t1.join();
+             t2.join();
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+         }
+
+         System.out.println("Tickets: "+count);
+         System.out.println("Tickets Left: "+c);
+    }
 }
